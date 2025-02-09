3643bab72891e1b4b89b363ee37f57c0e6410d04
salehweb-ir <salehweb.ir@gmail.com>
2025-02-09 17:32:16 +0330

    Update default.php

--- templates/default.php
+++ templates/default.php
@@ -4,25 +4,25 @@ if (!defined('ABSPATH')) {
 }
 
 // Include the send message functions from the includes folder
-include_once plugin_dir_path(__DIR__) . '/includes/send-message.php';
+include_once plugin_dir_path(__DIR__) . '/../includes/send-message.php';
 
-// نمایش فرم ارسال پیام
+// Display the form
 function wp2messenger_display_form() {
     if (isset($_POST['submit'])) {
-        // دریافت پیام از فرم ارسال شده
+        // Get the message from the form
         $message = sanitize_text_field($_POST['message']);
 
-        // دریافت توکن و آیدی کانال از تنظیمات
+        // Get the token and channel ID from the settings
         $token = get_option('token_eitaa_api');
         $channel_id = get_option('eitaa_channel_id');
 
-        // ارسال پیام به ایتا
+        // Send the message
         $send_result = wp2messenger_send_text_message($token, $channel_id, $message);
 
         if ($send_result) {
             echo '<div class="notice notice-success is-dismissible"><p>' . __('Message sent successfully.', 'wp2messenger') . '</p></div>';
         } else {
-            echo '<div class="notice notice-error is-dismissible"><p>' . __('Failed to send message. Try again later.', 'wp2messenger') . '</p></div>';
+            echo '<div class="notice notice-error is-dismissible"><p>' . __('Failed to send message. Please try again.', 'wp2messenger') . '</p></div>';
         }
     }
     ?>
